191d97ba38a238990411de10dfa7fb2a8f45eb24
Sushant Sikka <susikka@microsoft.com>
2019-10-22 14:43:57 +0530

    Adding Smart assist code sample

---
+++ omnichannel/smart-assist-bot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=smartassistcodesample-Web-Deploy.pubxml -p:Password=Z1pfzi4b1T4GvY647k0AzmtqePnXlkj3c8y2vTYhwJ5DsaHfk6Wu8dYFWmAZ
+
