ceed712a2c61ea229a6f61abcc0e2b7df741132f
Sushant Sikka <susikka@microsoft.com>
2020-02-11 16:04:31 +0530

    Adding code samples

---
+++ customer-service/channel-integration-framework/TwilioSampleInteg_version1/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TwilioSampleInteg.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
